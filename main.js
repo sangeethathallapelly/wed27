964e4bf2d9e5f2bd0602ec3d3a097634ff8d892d
sangeethathallapelly <146064909+sangeethathallapelly@users.noreply.github.com>
2023-09-27 17:39:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+function add(){
+    if(true){
+        var a="300";
+       
+        document.getElementById("varOutput").textContent=a;
+         a="500";
+         document.getElementById("varOutput1").textContent=a;
+    }  
+    }
+function change(){
+    if(true){
+        let b="300";
+      
+         document.getElementById("letOutput").textContent=b;
+        b="400";
+         document.getElementById("letOutput1").textContent=b;
+        
+       
+  
+    }
+}
+function sub(){
+   
+    if(true){
+         const c="Lion is the winner";
+       
+       
+        document.getElementById("constOutput").textContent=c;
+        
+    }
+}
+
+ 
